d702ad2bc63da247061c3722c6d7b82ab21b1303
alekzihz <AlexisGabriel.Diaz@autonoma.cat>
2024-12-30 16:58:10 +0100

    implementacion-basica-chatgpt

--- app/Http/Controllers/OpenAiController.php
+++ app/Http/Controllers/OpenAiController.php
@@ -4,12 +4,47 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use OpenAI;
+
 class OpenAiController extends Controller
 {
     //
 
-    public function index()
+    public function chat(Request $request)
     {
-        return response()->json(['message' => 'Hello OpenAITEST']);
+
+        //$message = $request->input('message');
+
+        $message = $request->json()->get('message');
+        if (!$message) {
+            return response()->json(['message' => 'Please provide a message']);
+        }
+
+        $openaiKey = env('OPENAI_KEY');
+
+        try {
+            $client = OpenAI::client($openaiKey);
+
+            $resultado = $client->chat()->create([
+                'model' => 'gpt-3.5-turbo',
+                'messages' => [
+                    ['role' => 'user', 'content' => 'Hello!'],
+                ],
+            ]);
+
+
+            $resultado = $resultado->choices[0]->message->content;
+            return response()->json([
+                'assistant' => $resultado
+            ]);
+
+
+            //$response = $openai->chat()->message($message)->send();
+            //return response()->json($response);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Error: ' . $e->getMessage()]);
+        }
+
+        // return response()->json(['message' => 'Hello OpenAITEST']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/openai', [OpenAiController::class, 'index']);
+Route::post('/openai', [OpenAiController::class, 'chat']);
 Route::get('/hola', function () {
     return response()->json(['message' => 'Hola Mundo']);
 });
